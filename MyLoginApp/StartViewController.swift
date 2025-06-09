fcdc3bd4d04bb0b20ee1ff5d460877f0235fd84e
maxminseok <pms950106@gmail.com>
2025-06-09 23:51:49 +0900

    [feat] SnapKit 의존성 추가

---
+++ MyLoginApp/StartViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StartViewController.swift
+//  MyLoginApp
+//
+//  Created by 박민석 on 6/9/25.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
