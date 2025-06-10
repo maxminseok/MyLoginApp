f563bd8d8205ea3427c44b6098a4fc25e7745ac0
maxminseok <pms950106@gmail.com>
2025-06-10 19:55:00 +0900

    [Feat] UserDTO 파일 추가

---
+++ MyLoginApp/UserDTO.swift
@@ -0,0 +1,14 @@
+//
+//  UserDTO.swift
+//  MyLoginApp
+//
+//  Created by 박민석 on 6/10/25.
+//
+
+import Foundation
+
+struct UserDTO {
+    let email: String
+    let password: String
+    let nickname: String
+}
